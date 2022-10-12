bf1c39b6ae3c630241c74a5b16bf0253b7ebe1cb
Maettis <mateo.meillon@ggl.schule>
2022-10-12 23:17:59 +0200

    Added timeout

--- backend/routes/nextcloud.js
+++ backend/routes/nextcloud.js
@@ -9,6 +9,7 @@ router.get('/login', async (req, res) => {
 
 router.get('/login/:token', async (req, res) => {
     // Run funtion every 2 seconds for the next 5 minutes
+    let started = Date.now()
     let interval = setInterval(async () => {
         const data = await fetch('https://nextcloud.b-sz-ggyl.logoip.de/index.php/login/v2/poll?token=' + req.params.token, {
             method: 'POST'
@@ -18,6 +19,11 @@ router.get('/login/:token', async (req, res) => {
         if (data.status == 200) {
             clearInterval(interval)
             res.status(200).send(await data.json())
+        }
+        // Stop after 5 minutes
+        if (Date.now() - started > 300000) {
+            clearInterval(interval)
+            res.status(500).send('Timeout')
         }
     }, 2000)
 })
